1b36f167683a8951e927253d285c99a3a3a62226
david_nicolay <david_nicolay>
2008-03-28 14:00:10 +0000

    Method getUntokenizedFieldNames moved from IndexWriterManager to CrawlerConfig

--- src/net/sf/regain/crawler/config/CrawlerConfig.java
+++ src/net/sf/regain/crawler/config/CrawlerConfig.java
@@ -294,4 +294,12 @@ public interface CrawlerConfig {
    */
   public Properties getCrawlerAccessControllerConfig();
   
+    /**
+   * Returns the names of the fields that shouldn't be tokenized.
+   * 
+   * @param config The crawler configuration.
+   * @return The names of the fields that shouldn't be tokenized.
+   */
+  public String[] getUntokenizedFieldNames();
+}
 }
